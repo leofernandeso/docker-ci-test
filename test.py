1bf782a09bcb9e0616fcd8e3bceb86d6234c14c2
leofernandeso <leonardo.fernandes.oliv@gmail.com>
2021-01-10 19:13:20 -0300

    included test

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ response = 200
 
 print("Running response status code test...")
 assert response == 200
+assert False, "Did not pass this test."
 print("Passed response status code test")
 print("Finished all tests. Bye.")
